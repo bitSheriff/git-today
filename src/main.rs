61efff5e7a8b22085cc8cc3aecb240101e61fd81
bitSheriff <root@bitsheriff.dev>
2025-09-08 17:39:14 +0200

    simple arg handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Command::new("git-today")
+        .disable_version_flag(true)
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Your Name <youremail@example.com>")
+        .about("A tool to recap your daily git work")
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(clap::ArgAction::Version)
+                .help("Print version information"),
+        )
+        .arg(
+            Arg::new("path")
+                .help("Path to the git repository")
+                .default_value("."),
+        )
+        .get_matches();
+
+    if let Some(path) = args.get_one::<String>("path") {
+        println!("Repository path: {}", path);
+    }
 }
