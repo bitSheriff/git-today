d8ef4c37cfea589c299c7f0256e1363999454890
bitSheriff <root@bitsheriff.dev>
2025-10-04 11:22:10 +0200

    test: added more info; fix some commit date problem

--- test/setup_repos.sh
+++ test/setup_repos.sh
@@ -5,17 +5,17 @@ function delete_all_repos() {
     rm -rf repos
 }
 
-function commit(){
+function commit() {
     local msg="$1"
     local date="$2" # optional date argument, e.g., "yesterday" or "2 days ago"
 
     # paste dev/urandom into dummy.txt
-    echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) > "dummy.txt"
+    echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) >"dummy.txt"
     git add "dummy.txt"
     if [ -z "$date" ]; then
         git commit -m "$msg"
     else
-        GIT_COMMITTER_DATE="$(date --date="$date")" git commit --date="$date" -m "$msg"
+        GIT_COMMITTER_DATE="$(LC_ALL=C date --date="$date")" git commit --date="$(LC_ALL=C date --date="$date")" -m "$msg"
     fi
 }
 
@@ -41,6 +41,7 @@ function create_repo() {
 ########################
 
 function testcase_a_simple() {
+    echo "----- Creating Repository a -----"
     create_repo "a"
     cd "a"
     commit "feat: init repo"
@@ -51,6 +52,7 @@ function testcase_a_simple() {
 }
 
 function testcase_b_branches() {
+    echo "----- Creating Repository b -----"
     create_repo "b"
     cd "b"
     commit "feat: init repo"
@@ -66,6 +68,7 @@ function testcase_b_branches() {
 }
 
 function testcase_c_merge() {
+    echo "----- Creating Repository c -----"
     create_repo "c"
     cd "c"
     commit "feat: init repo"
@@ -84,6 +87,7 @@ function testcase_c_merge() {
 }
 
 function testcase_d_authors() {
+    echo "----- Creating Repository d -----"
     create_repo "d"
     cd "d"
     commit "feat: init repo"
@@ -101,6 +105,7 @@ function testcase_d_authors() {
 }
 
 function testcase_e_time_based() {
+    echo "----- Creating Repository e -----"
     create_repo "e"
     cd "e"
     commit "docs: docs from 2 days ago" "2 days ago"
@@ -110,6 +115,7 @@ function testcase_e_time_based() {
 }
 
 function testcase_f_issue_numbers() {
+    echo "----- Creating Repository f -----"
     create_repo "f"
     cd "f"
     commit "[issue-123] docs: docs"
